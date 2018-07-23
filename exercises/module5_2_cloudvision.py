e5260adef52703bc1d2a41882bd202bc24b46fac
Alfred <angch@tertiaryinfotech.com>
2018-07-23 20:24:58 +0800

    0723

--- exercises/module5_2_cloudvision.py
+++ exercises/module5_2_cloudvision.py
@@ -11,7 +11,7 @@ client = vision.ImageAnnotatorClient()
 # The name of the image file to annotate
 file_name = os.path.join(
     os.path.dirname(__file__),
-    'resources/demo-image.jpg')
+    'resources/guess.jpg')
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
